50c9a794ebf084e06cea7bd2678e55ebcc37a4a1
Loiphan1003 <phanvuloi001@gmail.com>
2022-11-25 16:16:33 +0700

    #40-xong thong ke tai khoan

--- src/apis/baiTapLuyenTapAPI.js
+++ src/apis/baiTapLuyenTapAPI.js
@@ -29,8 +29,19 @@ const BaiTapLuyenTapAPI = {
         const url = `BTLuyenTap/EditBTLT?id=${id}&doKho=${doKho}&tieuDe=${tieuDe}&deBai=${deBai}&rangBuoc=${rangBuoc}&dinhDangDauVao=${dinhDangDauVao}&dinhDangDauRa=${dinhDangDauRa}&mauDauVao=${mauDauVao}&mauDauRa=${mauDauRa}&tag=${tag}`;
         console.log("API" + doKho);
         return axiosClient.put(url, {id}, {doKho}, {tieuDe}, {deBai}, {rangBuoc}, {dinhDangDauVao}, {dinhDangDauRa}, {mauDauVao}, {mauDauRa}, {tag});
+    },
+    SubmitBT: (valueSubmit) => {
+        const url = `BTLuyenTap/SubmitBTCode`;
+        return axiosClient.post(url, valueSubmit);
+    },
+    getBaiTapDaLam: (uId) => {
+        const url = `BTLuyenTap/getBaiTapDaLam?uId=${uId}`;
+        return axiosClient.get(url, {uId});
+    },
+    getAllOver: () => {
+        const url = 'BTLuyenTap/getAllOverView';
+        return axiosClient.get(url);
     }
-
 }
 
 export default BaiTapLuyenTapAPI;
